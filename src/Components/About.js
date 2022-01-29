d24ab2cbc10c425cb16de5ad4489c91a668ead71
Pruthvi@7384 <pruthvirajrajput575@gmail.com>
2022-01-29 16:09:08 +0530

    Assignment

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,7 +4,7 @@ import { Col, Container, Row } from 'react-bootstrap';
 function About() {
   return (
       <Container className="about_container">
-        <Row>
+        <Row className="align-items-center">
             <Col xl={8} className="text_containr">
                 <h4>WHAT WE DO</h4>
                 <h1>About Us & Our Work</h1>
